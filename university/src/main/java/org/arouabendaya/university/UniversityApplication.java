648b872df3a2662bbac9d39286db05795cdac505
aroua <aroua.bendaya@etudiant-fst.utm.tn>
2024-04-28 17:44:20 +0100

    Executer quelques requetes sur la base de données

--- university/src/main/java/org/arouabendaya/university/UniversityApplication.java
+++ university/src/main/java/org/arouabendaya/university/UniversityApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class UniversityApplication implements CommandLineRunner {
 
@@ -22,6 +24,32 @@ public class UniversityApplication implements CommandLineRunner {
 		productRepository.save(new Product(null,"Computer",4300,3));
 		productRepository.save(new Product(null,"Printer",1200,4));
 		productRepository.save(new Product(null,"Smart Phone",3200,32));
+		List<Product> products= productRepository.findAll();
+		products.forEach(p->System.out.println(p.toString()));
+
+		Product product=productRepository.findById(Long.valueOf(1)).get();
+		System.out.println("***********");
+		System.out.println(product.getId());
+		System.out.println(product.getName());
+		System.out.println(product.getQuantity());
+		System.out.println("***********");
+		System.out.println("-----------");
+		List<Product>productList =productRepository.findByNameContains("C");
+		productList.forEach(p ->{
+			System.out.println(p.toString());
+		} );
+
+		System.out.println("------------");
+		List<Product>productList2 =productRepository.search("%C%");
+		productList2.forEach(System.out::println);
+
+		System.out.println("------------");
+		List<Product>productList3 =productRepository.findByPriceGreaterThan(2);
+		productList3.forEach(System.out::println);
+
+		System.out.println("------------");
+		List<Product>productList4 =productRepository.findByPriceGreaterThan(2);
+		productList4.forEach(System.out::println);
 
 	}
 }
